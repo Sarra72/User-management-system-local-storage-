dae7d5b24c71ff8f8821fc58bf4daf1085161dc9
Sara AbdelKader <sarahmedd72@gmail.com>
2025-05-18 19:16:37 +0300

    Add files via upload

---
+++ src/components/DisplayUsers.jsx
@@ -0,0 +1,59 @@
+
+import { useState } from "react";
+
+export default function DisplayUsers({id ,name ,email ,getUsers}) {
+    let [updatedName,setUpdatedName] =useState(name);
+    let [updatedEmail,setUpdatedEmail] =useState(email);
+    let [isUpdated , setIsUpdated]= useState(false);
+
+ 
+   function deleteUser(e, id) {
+        e.preventDefault();
+        const users = JSON.parse(localStorage.getItem("users"));
+        const updatedUsers = users.filter(user => user.id !== id);
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+        getUsers();
+    }
+
+    function updateUser(e, id) {
+        e.preventDefault();
+        const users = JSON.parse(localStorage.getItem("users"));
+        const updatedUsers = users.map(user =>
+            user.id === id ? { ...user, name: updatedName, email: updatedEmail } : user
+        );
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+        setIsUpdated(false);
+        getUsers();
+    }
+   
+  return (
+    <>
+    
+    <div className="all ">
+        {
+        isUpdated?(
+            <div className="true ">
+            <input type="text" value={id} readOnly className="form-items "/>
+            <input type="text" value={updatedName} onChange={(e)=>{setUpdatedName(e.target.value)}} className="form-items "/>
+            <input type="email" value={updatedEmail} onChange={(e)=>{setUpdatedEmail(e.target.value)}} className="form-items "/>
+
+            <button className="save-btn " onClick={(e)=>{updateUser(e,id)}}>Save</button>
+            <button className="delete-btn" onClick={(e)=>{deleteUser(e,id)}} >Delete</button>
+            </div>
+        ):(
+            <div className="false ">
+                <span className="spans ">{id}</span>
+                <span className="spans "> {name} </span>
+                <span className="spans "> {email} </span>
+                
+                <button className="update-btn " onClick={()=>{setIsUpdated(true)}}>Update</button>
+                <button className="delete-btn " onClick={(e)=>{deleteUser(e,id)}} >Delete</button>
+            </div>
+        )
+    }
+      
+      
+    </div>
+    </>
+  )
+}
---
+++ src/components/Users.jsx
@@ -0,0 +1,58 @@
+
+import { useEffect, useState } from "react";
+
+import DisplayUsers from "./DisplayUsers";
+
+export default function Users() {
+    let [addedName , setAddedName] =useState("");
+    let [addedEmail , setAddedEmail] =useState("");
+    let [users , setUsers] =useState([]);
+
+    function getUsers() {
+        const storedUsers = JSON.parse(localStorage.getItem("users")) ;
+        setUsers(storedUsers);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    function addUser(e) {
+        e.preventDefault();
+        const newUser = {id: Date.now(), name: addedName, email: addedEmail };
+        const updatedUsers = [...users, newUser];
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+        setAddedName("");
+        setAddedEmail("");
+        getUsers();
+    }
+
+  return (
+
+    <>
+    <h1 className="hd-o ">Users Management System</h1>
+    <div className="all text-lg">
+        <h2 className="hd">Add Users</h2>
+        <form onSubmit={(e)=>{addUser(e)}} className="form ">
+            <input type="text" placeholder="Name" value={addedName} onChange={(e)=>{setAddedName(e.target.value)}} required className="form-items "/>
+            <input type="email" placeholder="Email" value={addedEmail} onChange={(e)=>{setAddedEmail(e.target.value)}} required className="form-items "/>
+            <button type="submit" className="form-btn "> ADD </button>
+        </form>
+    </div>
+    <div className="p-8"></div>
+      
+      <h2 className="hd">Display Users</h2>
+        <div className="all true">
+            <span>ID</span>
+            <span>Name</span>
+            <span>Email</span>
+        </div>
+      {
+        users.length === 0 ? ( <h2 className="all no-users">No users</h2>) : 
+        (users.map((user)=>{   
+            return <DisplayUsers key={user.id} id={user.id} name={user.name} email={user.email} getUsers={getUsers}/>
+        }))
+      }
+    </>
+  )
+}
